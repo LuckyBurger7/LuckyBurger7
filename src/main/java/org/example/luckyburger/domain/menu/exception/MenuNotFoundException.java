45d4eb8b9e64bd4e7a2fe638ae11cc3a3a3664fd
donghyeon505 <ehdgus458@gmail.com>
2025-10-20 19:13:01 +0900

    refactor: Exception 클래스 이름 변경 #10

--- src/main/java/org/example/luckyburger/domain/menu/exception/MenuNotFoundException.java
+++ src/main/java/org/example/luckyburger/domain/menu/exception/MenuNotFoundException.java
@@ -3,8 +3,8 @@ package org.example.luckyburger.domain.menu.exception;
 import org.example.luckyburger.common.exception.GlobalException;
 import org.example.luckyburger.domain.menu.code.MenuErrorcode;
 
-public class NotFoundMenuException extends GlobalException {
-    public NotFoundMenuException() {
+public class MenuNotFoundException extends GlobalException {
+    public MenuNotFoundException() {
         super(MenuErrorcode.MENU_NOT_FOUND);
     }
 }
