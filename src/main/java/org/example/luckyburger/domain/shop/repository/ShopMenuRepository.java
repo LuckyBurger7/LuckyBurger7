9db774452b3999236492d84ce4cf5f296d0d59d7
donghyeon505 <ehdgus458@gmail.com>
2025-10-22 15:48:51 +0900

    refactor: 필요없는 @Param 제거 및 쿼리 작성 #43

--- src/main/java/org/example/luckyburger/domain/shop/repository/ShopMenuRepository.java
+++ src/main/java/org/example/luckyburger/domain/shop/repository/ShopMenuRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,8 +14,5 @@ public interface ShopMenuRepository extends JpaRepository<ShopMenu, Long> {
     List<ShopMenu> getAllByShopId(Long shopId);
 
     @EntityGraph(attributePaths = {"menu"})
-    Page<ShopMenu> findByShopIdAndStatusIsNot(
-            @Param("shopId") Long shopId,
-            @Param("status") ShopMenuStatus status,
-            Pageable pageable);
+    Page<ShopMenu> findByShopIdAndStatusIsNot(Long shopId, ShopMenuStatus status, Pageable pageable);
 }
--- src/main/java/org/example/luckyburger/domain/shop/repository/ShopRepository.java
+++ src/main/java/org/example/luckyburger/domain/shop/repository/ShopRepository.java
@@ -4,8 +4,11 @@ import org.example.luckyburger.domain.shop.entity.Shop;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
 
-    Page<Shop> findByNameContaining(String shopName, Pageable pageable);
+    @Query("SELECT s FROM Shop s " +
+            "WHERE (:shopname IS NULL OR s.name LIKE CONCAT('%', :shopname, '%'))")
+    Page<Shop> findAllByNameContaining(String shopName, Pageable pageable);
 }
