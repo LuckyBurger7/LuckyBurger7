ee4b11f686a29eb391afa22dc49b0eae706e3590
doldollee00 <doldollee00@gmail.com>
2025-10-23 13:55:17 +0900

    refactor: 리뷰 예외처리 수정, 이벤트 조회 도메인 수정, 점포 도메인명 수정 #57

--- src/main/java/org/example/luckyburger/domain/event/controller/EventAdminController.java
+++ src/main/java/org/example/luckyburger/domain/event/controller/EventAdminController.java
@@ -2,19 +2,25 @@ package org.example.luckyburger.domain.event.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.luckyburger.common.dto.response.ApiPageResponse;
 import org.example.luckyburger.common.dto.response.ApiResponse;
 import org.example.luckyburger.domain.auth.enums.AccountRole;
 import org.example.luckyburger.domain.event.dto.request.EventCreateRequest;
 import org.example.luckyburger.domain.event.dto.response.EventResponse;
 import org.example.luckyburger.domain.event.service.EventAdminService;
+import org.example.luckyburger.domain.event.service.EventService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,6 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventAdminController {
 
     private final EventAdminService eventAdminService;
+    private final EventService eventService;
 
     @PostMapping("/v1/admin/events")
     public ResponseEntity<ApiResponse<EventResponse>> createEvent(
@@ -44,5 +51,15 @@ public class EventAdminController {
     public ResponseEntity<ApiResponse<Void>> deleteEvent(@PathVariable Long eventId) {
         eventAdminService.deleteEvent(eventId);
         return ApiResponse.noContent();
+    }
+
+    @GetMapping("/v1/admin/events/all")
+    public ResponseEntity<ApiPageResponse<EventResponse>> getAllEvent(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        return ApiPageResponse.success(eventService.getAllEventResponse(pageable));
     }
 }
