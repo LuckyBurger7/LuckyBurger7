09d20cc8ea7321108f85e13069bbea0e33dd3132
kisu kim <qpsrltn@gmail.com>
2025-10-24 14:39:05 +0900

    fix: 충돌 오류 수정 #59

--- src/main/java/org/example/luckyburger/domain/menu/service/MenuEntityFinder.java
+++ src/main/java/org/example/luckyburger/domain/menu/service/MenuEntityFinder.java
@@ -9,6 +9,8 @@ import org.example.luckyburger.domain.menu.repository.MenuRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @Transactional(readOnly = true)
