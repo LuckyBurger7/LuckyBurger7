f4e05d83b565ee0ad7b81d8756d518e7632b1eeb
donghyeon505 <ehdgus458@gmail.com>
2025-10-24 11:25:54 +0900

    refactor: response에 id 추가

--- src/main/java/org/example/luckyburger/domain/shop/dto/response/ShopDashboardResponse.java
+++ src/main/java/org/example/luckyburger/domain/shop/dto/response/ShopDashboardResponse.java
@@ -4,15 +4,18 @@ import lombok.Builder;
 
 @Builder
 public record ShopDashboardResponse(
+        Long shopId,
         Integer todayOrderCount,
         Long todayTotalSales,
         Double averageRating
 ) {
     public static ShopDashboardResponse of(
+            Long shopId,
             Integer todayOrderCount,
             Long todayTotalSales,
             Double averageRating) {
         return ShopDashboardResponse.builder()
+                .shopId(shopId)
                 .todayOrderCount(todayOrderCount)
                 .todayTotalSales(todayTotalSales)
                 .averageRating(averageRating)
--- src/main/java/org/example/luckyburger/domain/shop/dto/response/ShopResponse.java
+++ src/main/java/org/example/luckyburger/domain/shop/dto/response/ShopResponse.java
@@ -6,14 +6,17 @@ import org.example.luckyburger.domain.shop.entity.Shop;
 import org.example.luckyburger.domain.shop.enums.BusinessStatus;
 
 @Builder
-public record ShopResponse (String name,
-                            BusinessStatus businessStatus,
-                            String address,
-                            String street){
+public record ShopResponse(
+        Long shopId,
+        String name,
+        BusinessStatus businessStatus,
+        String address,
+        String street) {
 
-    public static ShopResponse from(Shop shop){
+    public static ShopResponse from(Shop shop) {
 
         return ShopResponse.builder()
+                .shopId(shop.getId())
                 .name(shop.getName())
                 .businessStatus(shop.getStatus())
                 .address(shop.getAddress())
