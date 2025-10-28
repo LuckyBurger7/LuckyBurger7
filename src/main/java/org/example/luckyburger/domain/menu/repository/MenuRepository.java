6d223be382c4e3a962878525a72616a9b92b7ff4
doldollee00 <doldollee00@gmail.com>
2025-10-28 11:38:37 +0900

    refactor: 더미데이터 입력 시 DB존재 여부 체크 #87

--- src/main/java/org/example/luckyburger/domain/menu/repository/MenuRepository.java
+++ src/main/java/org/example/luckyburger/domain/menu/repository/MenuRepository.java
@@ -1,14 +1,17 @@
 package org.example.luckyburger.domain.menu.repository;
 
+import java.util.List;
+import java.util.Optional;
 import org.example.luckyburger.domain.menu.entity.Menu;
 import org.example.luckyburger.domain.menu.enums.MenuCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     @Query("SELECT m.id FROM Menu m WHERE m.category=:category")
     List<Long> findAllByCategory(@Param("menuCategory") MenuCategory category);
+
+    // 더미 데이터 체크
+    Optional<Menu> findByName(String name);
 }
