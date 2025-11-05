0fbddaab5697613e47121d7fbfd7b29720c87c9d
doldollee00 <doldollee00@gmail.com>
2025-11-05 12:30:29 +0900

    refactor: menuCategory 파라미터명 오타 수정 #93

--- src/main/java/org/example/luckyburger/domain/menu/repository/MenuRepository.java
+++ src/main/java/org/example/luckyburger/domain/menu/repository/MenuRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-    @Query("SELECT m.id FROM Menu m WHERE m.category=:category")
+    @Query("SELECT m.id FROM Menu m WHERE m.category=:menuCategory")
     List<Long> findAllByCategory(@Param("menuCategory") MenuCategory category);
 
     // 더미 데이터 체크
