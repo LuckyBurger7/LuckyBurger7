b630885a321392dccba232fba74dfba4347413d0
kisu kim <qpsrltn@gmail.com>
2025-11-13 19:06:15 +0900

    fix: DB Update로 변경

--- src/main/java/org/example/luckyburger/LuckyBurgerApplication.java
+++ src/main/java/org/example/luckyburger/LuckyBurgerApplication.java
@@ -1,10 +1,9 @@
 package org.example.luckyburger;
 
-import org.redisson.spring.starter.RedissonAutoConfigurationV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {RedissonAutoConfigurationV2.class})
+@SpringBootApplication
 public class LuckyBurgerApplication {
 
     public static void main(String[] args) {
