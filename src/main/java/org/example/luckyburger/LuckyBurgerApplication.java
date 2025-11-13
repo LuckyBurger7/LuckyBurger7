cdfce7ef01b7afb192e47a5b96550676cf18096d
kisu kim <qpsrltn@gmail.com>
2025-11-13 17:54:15 +0900

    fix: @SpringBootApplication(exclude = {RedissonAutoConfigurationV2.class})

--- src/main/java/org/example/luckyburger/LuckyBurgerApplication.java
+++ src/main/java/org/example/luckyburger/LuckyBurgerApplication.java
@@ -1,9 +1,10 @@
 package org.example.luckyburger;
 
+import org.redisson.spring.starter.RedissonAutoConfigurationV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedissonAutoConfigurationV2.class})
 public class LuckyBurgerApplication {
 
     public static void main(String[] args) {
